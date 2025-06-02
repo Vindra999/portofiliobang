47f3dcd5e74fc0d25ad07c69668a6f89929f8efa
Vindra999 <nopindraaugustiar@gmail.com>
2025-06-02 15:41:57 +0700

    as

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -4,4 +4,10 @@ import { about, person } from "@/app/resources/content";
 import { Meta, Schema } from "@/once-ui/modules";
 import { Projects } from "@/components/work/Projects";
 
+export async function generateMetadata() {
+return null
+}
 
+export default function Work() {
+  return null
+}
